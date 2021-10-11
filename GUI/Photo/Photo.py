ea25e5184eedd85bad289e4e5e857d460d312e0f
EduardoS22 <90275733+EduardoS22@users.noreply.github.com>
2021-10-11 13:44:53 +0200

    Add files via upload

--- GUI/Photo/Photo.py
+++ GUI/Photo/Photo.py
@@ -4,7 +4,7 @@
 @author: eduardo.sanz
 """
 
-from skimage import io
+#from skimage import io
 #from skimage import transform as tf
 from skimage.feature import (match_descriptors, corner_peaks, corner_harris, plot_matches, BRIEF, ORB)
 import cv2 as cv
@@ -14,25 +14,27 @@ class Photo():
 
         photo_path = None    
         image = None
+        gray = None
     
-        def __init__(self, photo_path):
-            self.photo_path = photo_path
+        def __init__(self, image):
+            self.image = image
+            self.gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
             
         def getKeyPoints(self):
             'Función para obtener la lista de keypoints de la imagen'
             
-            keypoints_photo = corner_peaks(corner_harris(self.photo_path), min_distance=20)
+            keypoints = corner_peaks(corner_harris(self.gray), min_distance=20)
             brief = BRIEF()
-            brief.extract(self.photo,keypoints_photo)
-            keypoints_photo = keypoints_photo[brief.mask]
-            descriptor_photo = brief.descriptors
+            brief.extract(self.gray,keypoints)
+            keypoints = keypoints[brief.mask]
+            descriptor = brief.descriptors
             
             #para comparar los descriptores de las imagenes
             #matchesDescriptors = match_descriptors(descriptor_photo, descriptor_imgGray)
             
-            return descriptor_photo
+            return descriptor
             
-        
+        """
         def newPhoto(self):
              
             self.image = cv.imread(self.photo_path, cv.IMREAD_GRAYSCALE)
@@ -43,7 +45,7 @@ class Photo():
             self.image = cv.cvtColor(self.image,cv.COLOR_BGR2RGB)
             
             return  self.image
-            
+        """ 
     
         def findContour(self):
             'Función para obtener los contornos de la imagen'
@@ -51,8 +53,7 @@ class Photo():
             'variable para guardar los pixeles donde esta la señal'
             cont = []
             
-            gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
-            _, threshold = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+            _, threshold = cv.threshold(self.gray, 127, 255, cv.THRESH_BINARY)
             contours, _ = cv.findContours(threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
             i = 0
             for contour in contours:
